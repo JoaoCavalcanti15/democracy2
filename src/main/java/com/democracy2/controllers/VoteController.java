9195d846a023c57d0c80d1fa0be47c6d49f8c4f8
jaojao <uni.joaoassis@gmail.com>
2024-05-27 18:46:33 +0100

    Primeira fase do projeto

--- src/main/java/com/democracy2/controllers/VoteController.java
+++ src/main/java/com/democracy2/controllers/VoteController.java
@@ -5,6 +5,8 @@ import com.democracy2.services.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class VoteController {
     public ResponseEntity<List<Vote>> getOngoingVotes() {
         List<Vote> votes = voteService.getOngoingVotes();
         return ResponseEntity.ok(votes);
+    }
+
+    @PostMapping("/close/{voteId}")
+    public ResponseEntity<Void> closeVote(@PathVariable Long voteId) {
+        voteService.closeVote(voteId);
+        return ResponseEntity.noContent().build();
     }
 }
